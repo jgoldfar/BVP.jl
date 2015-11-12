f064684d3de955a5c7745243bc68d7f091d71398
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-11-11 20:27:35 -0500

    Use BaseTestNext, the new test framework for Julia v0.5, which offers better reporting than the previous Base test framework.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,38 +4,38 @@ if VERSION < v"0.4-"
   typealias LinSpace{T} Vector{T}
 end
 using Compat
-typealias GridType{T} Union(Vector{T}, LinSpace{T})
+typealias GridType{T} Union{Vector{T}, LinSpace{T}}
+const pkgbasedir = joinpath(dirname(@__FILE__), "..")
 # include(joinpath(dirname(@__FILE__), "..", "src", "bvp.jl"))
-include(joinpath(dirname(@__FILE__), "..", "src", "odesolve.jl"))
-include(joinpath(dirname(@__FILE__), "..", "src", "colloc.jl"))
-include(joinpath(dirname(@__FILE__), "..", "src", "shooting.jl"))
-include(joinpath(dirname(@__FILE__), "..", "src", "fem.jl"))
+include(joinpath(pkgbasedir, "src", "odesolve.jl"))
+include(joinpath(pkgbasedir, "src", "colloc.jl"))
+include(joinpath(pkgbasedir, "src", "shooting.jl"))
+include(joinpath(pkgbasedir, "src", "fem.jl"))
 
-using Base.Test
-test_handler(r::Test.Success) = print_with_color(:green, ".")
-const rpt_fails = false
-test_handler(r::Test.Failure) = (print_with_color(:red, "."); rpt_fails && println(STDERR, "\n", r))
-test_handler(r::Test.Error) = (print_with_color(:red, "x"); println(STDERR, "\n", r))
-const testfiles = (
-  "mlcompare.jl",
-  "fdm.jl",
-  "trapz.jl",
-  "shooting.jl",
-  "fem.jl"
-  )
+if VERSION >= v"0.5-"
+  using Base.Test
+else
+  using BaseTestNext
+  const Test = BaseTestNext
+end
 
 tic()
-Test.with_handler(test_handler) do
-  for testfile in testfiles
-    try
-      println(" Running test $testfile.\n")
-      include(joinpath(dirname(@__FILE__),testfile))
-      println("\n")
-    catch v
-      println(" Error $v\n in $testfile\n")
-      Base.show_backtrace(STDERR, catch_backtrace())
-      print("\n")
-    end
+@testset "All" begin
+  @testset "MATLAB Compare" begin
+  include("mlcompare.jl")
+  @test true
+  end
+  @testset "FDM" begin
+  include("fdm.jl")
+  end
+  @testset "Trapz" begin
+  include("trapz.jl")
+  end
+  @testset "Shooting" begin
+  include("shooting.jl")
+  end
+  @testset "FEM" begin
+  include("fem.jl")
   end
 end
 toc()
